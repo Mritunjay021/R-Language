d8fef092963156d40a344a16d6af6fc1151f58da
Mritunjay021 <mrityurk21@gmail.com>
2024-06-02 17:31:35 +0530

    Create day2.r

---
+++ day2.r
@@ -0,0 +1,40 @@
+cat(c2k3,,v1)
+cat(c2k3,v,v1)
+a<-1=
+a<-10  
+a=10
+b<-10
+a=10
+b=20
+a<-4
+b=5
+c<-a+b
+print(c)
+cat("sum is",c)
+print("sum is",c)
+ls()
+print(ls())
+rm(var__nam)
+rm(list=ls())
+a=10
+b="true"
+c=TRUE
+v=16
+print(a*v)
+print (a+v)
+print (a-v)
+print (a/v)
+print (a^v)
+print (a%%v)
+print (a%/%v)
+print (a>v)
+print (a<v)
+print (a==v)
+print (a>=v)
+print (a<=v)
+print (a!=v)
+print (a&v)
+print (!a)
+print (a|v)
+print (a&&v)
+print (a||v)
