c5a0f5ddb0e9ab4d839f5a2832ec58646a76a8a5
Mritunjay021 <mrityurk21@gmail.com>
2024-06-03 20:32:54 +0530

    r

---
+++ day3.r
@@ -0,0 +1,42 @@
+a=5
+a<-4
+5->a
+a<<-3.4
+c(1,2)->>a
+a:v
+print(a:v)
+a=6
+print(a:v)
+print(a%in%b)
+print(a%*%t(a))
+print("abc")
+print(12.5)
+print(63L)
+print(TRUE)
+print(2+3i)
+print(charToRaw('hello'))
+a<-c(1,2,3,5,6)
+b<-c(3:10)
+class(b)
+class(a)
+v1<-c('a','d')
+v2<-c(6:10)
+print(v1*v2)
+print (v1+v2)
+print (v1-v2)
+print (v1/v2)
+print (v1^v2)
+print (v1%%2)
+print (v1%/%v2)
+print (v1>v2)
+print (v1<v2)
+print (v1==v2)
+print (v1>=v2)
+print (v1<=v2)
+print (v1!=v2)
+print (v1&v2)
+print (!v1)
+print (v1|v2)
+print (v1&v2)
+print (v1|v2)
+class(v2)
